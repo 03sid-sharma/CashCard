2acd3c8b681f2cc1d4557f01575565e427315573
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2024-10-25 23:31:26 +0530

    feat: repository

---
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -0,0 +1,8 @@
+package example.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+@SuppressWarnings("rawtypes")
+public interface CashCardRepository extends CrudRepository{
+
+}
