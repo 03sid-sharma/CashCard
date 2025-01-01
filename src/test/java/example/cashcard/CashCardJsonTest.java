4d1cb89772d486bc82d0fab45274edbaf18425ca
Siddharth Sharma <03sid.sharma@gmail.com>
2025-01-02 05:11:05 +0530

    feat: list serialization test

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -25,8 +25,27 @@ class CashCardJsonTest {
     void setUp(){
         cashCards = Arrays.array(
                 new CashCard(99L, 123.45),
-                new CashCard(100L, 100.00),
+                new CashCard(100L, 1.00),
                 new CashCard(101L, 150.00));
+    }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException{
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
+
+    @Test
+    void cashCardListDeSerializationTest() throws IOException{
+        String expectedString = """
+                [
+                  {"id": 99, "amount": 123.45 },
+                  {"id": 100, "amount": 1.00 },
+                  {"id": 101, "amount": 150.00 }
+                ]
+                """;
+
+        assertThat(jsonList.parse(expectedString)).isEqualTo(cashCards);
+
     }
 
     @Test
