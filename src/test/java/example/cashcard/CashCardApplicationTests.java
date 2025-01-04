faeb53851d59f8b93c16250a30a45aeea752df86
Siddharth Sharma <03sid.sharma@gmail.com>
2025-01-04 17:45:25 +0530

    test: add integration test for retrieving all cash cards

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,20 +1,27 @@
 package example.cashcard;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+
+import net.minidev.json.JSONArray;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.net.URI;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 
 	@Autowired
@@ -61,4 +68,23 @@ class CashCardApplicationTests {
 	// assertThat(amount).isEqualTo(250.00);
 	// }
 
+	@Test
+	void shouldReturnAllCashCardsWhenListIsRequested(){
+		// We have 1 cashcard created above for 250 amount rest 2 we'll add :)
+		restTemplate.postForEntity("/cashcards", new CashCard(null, 123.45), Void.class);
+		restTemplate.postForEntity("/cashcards", new CashCard(null, 1.00), Void.class);
+
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class); 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		int cashCardCount = documentContext.read("$.length()");
+		assertThat(cashCardCount).isEqualTo(3);
+
+		JSONArray ids = documentContext.read("$..id");
+		assertThat(ids).containsExactlyInAnyOrder(1, 2, 3);
+
+		JSONArray amounts = documentContext.read("$..amount");
+		assertThat(amounts).containsExactlyInAnyOrder(250.00, 1.00, 123.45);
+	}
 }
